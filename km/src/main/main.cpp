973c79fb22b750e38d7488f781703498c582af00
soamane <soamane@mail.ru>
2025-04-18 03:15:47 +0300

    update km

---
+++ km/src/main/main.cpp
@@ -0,0 +1,10 @@
+﻿#include <ntddk.h>
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
+	UNREFERENCED_PARAMETER(DriverObject);
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	KdPrint(("[Driver] Called DriverEntry"));
+
+	return STATUS_SUCCESS;
+}
